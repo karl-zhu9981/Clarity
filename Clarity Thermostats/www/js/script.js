12b371e3e4acf3a95f9654fb4bc0dfa3b2fa20d3
juliansamek <juliansamek00@gmail.com>
2017-08-21 20:36:45 -0400

    Improves navbar tabs

--- Clarity Thermostats/www/js/script.js
+++ Clarity Thermostats/www/js/script.js
@@ -17,18 +17,47 @@ $(document).ready(function(){
     /* this if for the tabs */
     var activeTab = '#home-tab';
     $(activeTab).addClass('nav-item-active');
+    changeTab(activeTab);
 });
 
+/* this function works for changing the temperature */
 function changeTemp (temp) {
     /* allows you to increase temperature of house */
-    $("#temp-up-button").click(function() {
+    $('#temp-up-button').click(function() {
         temp ++;
         $('#main-temp').text(temp);
     });
 
     /* allows you to decrease temperature */
-    $("#temp-down-button").click(function() {
+    $('#temp-down-button').click(function() {
         temp --;
         $('#main-temp').text(temp);
+    });
+};
+
+/* this function applies for changing the tab on the navbar */
+function changeTab (activeTab) {
+    $('#home-tab').click(function() {
+        $(activeTab).removeClass('nav-item-active');
+        activeTab = '#home-tab';
+        $(activeTab).addClass('nav-item-active');
+    });
+
+    $('#comparisons-tab').click(function() {
+        $(activeTab).removeClass('nav-item-active');
+        activeTab = '#comparisons-tab';
+        $(activeTab).addClass('nav-item-active');
+    });
+
+    $('#statistics-tab').click(function() {
+        $(activeTab).removeClass('nav-item-active');
+        activeTab = '#statistics-tab';
+        $(activeTab).addClass('nav-item-active');
+    });
+
+    $('#settings-tab').click(function() {
+        $(activeTab).removeClass('nav-item-active');
+        activeTab = '#settings-tab';
+        $(activeTab).addClass('nav-item-active');
     });
 };
